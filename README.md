7ec1861696d7ddc8d8160daf01b466fbc70d32d4
Tim Mikeladze <tim.mikeladze@gmail.com>
2021-02-13 20:18:41 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# use-spotify
+React hooks for the Spotify Web API
+
+## Installation
+
+```shell
+yarn add use-spotify
+
+npm install use-spotify
+```
+
+## Usage
+
+1. Wrap your components with a `SpotifyApiProvider`.
+2. Consume the `useSpotify` or `useSpotifyLazy` hooks.
+
+## Examples 
+
+```typescript jsx
+import { useSpotify } from "use-spotify";
+
+const App = () => {
+    return (
+        <SpotifyApiProvider accessToken={'token goes here'}>
+            <MyTopArtists/>
+            <Search />
+        </SpotifyApiProvider>
+    );
+};
+```
+
+```typescript jsx
+const MyTopArtists = () => {
+    const myTopArtists = useSpotify('getMyTopArtists');
+    
+    if (myTopArtists.loading) {
+        return 'Loading';
+    }
+    
+    return myTopArtists.data.artists.items.map(x => (
+        <div key={x.id}>
+            {x.name}
+        </div>
+    ))
+}
+```
+
+```typescript jsx
+const Search = () => {
+    const search = useSpotify(
+        'search',
+        {
+            limit: 5,
+        },
+        'The National',
+        ['artist', 'album']
+    );
+
+    if (search.loading) {
+        return 'Loading';
+    }
+
+    return search.data.artists.items.map(x => (
+        <div key={x.id}>
+            {x.name}
+        </div>
+    ))
+}
+```
+
+```typescript jsx
+const SearchLazy = () => {
+    const [search, results] = useSpotifyLazy(
+        'search',
+    );
+
+    useEffect(() => {
+        (async () => {
+            const searchResults = await search(
+                'search',
+                {
+                    limit: 5,
+                },
+                'The National',
+                ['artist', 'album']
+            );
+        })()
+    }, [])
+    
+    if (results.loading) {
+        return 'Loading';
+    }
+
+    return results.data.artists.items.map(x => (
+        <div key={x.id}>
+            {x.name}
+        </div>
+    ))
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,94 @@
+import React, {
+  useCallback, useContext, useEffect, useState,
+} from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { FunctionKeys } from 'utility-types';
+
+export type SpotifyApiContextInterface = {
+    spotifyApi: SpotifyWebApi.SpotifyWebApiJs;
+};
+
+export const SpotifyApiContext = React.createContext<
+    Partial<SpotifyApiContextInterface>
+    >({});
+
+export interface SpotifyApiProviderProps {
+    accessToken: string;
+    children?: React.ReactNode;
+}
+
+export const SpotifyApiProvider = (props: SpotifyApiProviderProps) => {
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(props.accessToken);
+
+  return (
+    <SpotifyApiContext.Provider value={{ spotifyApi }}>
+      {props.children}
+    </SpotifyApiContext.Provider>
+  );
+};
+
+export const useSpotify = (
+  method: FunctionKeys<SpotifyWebApi.SpotifyWebApiJs>,
+  options = {},
+  ...args
+) => {
+  const { spotifyApi } = useContext(SpotifyApiContext);
+
+  const [res, setRes] = useState({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    spotifyApi[method](...args, options, (error, data) => {
+      setRes({
+        data,
+        error,
+        loading: false,
+      });
+    });
+  }, []);
+
+  return res;
+};
+
+export const useSpotifyLazy = (
+  method: FunctionKeys<SpotifyWebApi.SpotifyWebApiJs>,
+  options = {},
+  ...args
+) => {
+  const { spotifyApi } = useContext(SpotifyApiContext);
+
+  const [res, setRes] = useState({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  const callback = useCallback(async (overrideOptions?, ...overrideArgs) => {
+    let data;
+    let error;
+
+    try {
+      if (overrideArgs || args) {
+        data = await spotifyApi[method](...(overrideArgs || args), overrideOptions || options);
+      } else {
+        data = await spotifyApi[method](overrideOptions);
+      }
+    } catch (err) {
+      error = err;
+    }
+
+    setRes({
+      data,
+      error,
+      loading: false,
+    });
+
+    return { data, error, loading: false };
+  }, []);
+
+  return [callback, res];
+};
